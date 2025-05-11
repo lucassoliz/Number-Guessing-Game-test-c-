85ad6ba09746c7b898f8aedf65c671db71613d55
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2025-05-11 19:02:26 -0300

    FUNCION: iniciar_juego

---
+++ juego.c
@@ -0,0 +1,12 @@
+#include "juego.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void iniciar_juego() {
+    printf("¡Bienvenido al juego de adivinar!\n");
+    printf("Elige un modo:\n");
+    printf("1. Adivinar un número\n");
+    printf("2. Adivinar una palabra\n");
+}
